e7fa216282c245735750afee4116adc202639885
Geiler Elias Radillo Sarmiento <geilerelias@gmail.com>
2025-05-27 14:59:50 -0500

    actualizaciones

---
+++ resources/js/Layouts/AuthLayout.vue
@@ -0,0 +1,118 @@
+<script setup>
+import {useDisplay} from "vuetify";
+import {Head, Link} from "@inertiajs/vue3";
+import logo from "@images/favicon/favicon-96x96.png";
+import name from "@images/favicon/favicon-96x96.png";
+import {onMounted} from "vue";
+import AOS from "aos";
+import lazyBg1 from "@images/bg/lazy-home.png";
+import bg1 from "@images/bg/bg-home-2.png";
+import bgAbout from "@images/bg/bg-about.jpg";
+
+defineProps({
+    banner: {
+        type: Boolean,
+        default: true,
+    },
+    title: String,
+});
+
+onMounted(() => {
+    AOS.init();
+});
+
+const {smAndDown} = useDisplay();
+</script>
+
+<template>
+    <v-app>
+        <Head>
+            <title>{{ title }}</title>
+        </Head>
+
+        <v-row class="pa-0 ma-0" no-gutters>
+           <v-col
+                v-if="banner"
+                :order="smAndDown ? '2' : '1'"
+                class="d-flex flex-column-reverse align-center justify-center my-0 py-0 align-center relative"
+                cols="12"
+                lg="7"
+                md="6"
+                style="position: sticky; left: 0; top: 0; height: 100vh; z-index: 2; overflow: hidden;"
+            >
+                <v-img
+                    :lazy-src="lazyBg1"
+                    :src="bg1"
+                    alt="bg lg"
+                    class="z-index2 h-50"
+                    cover
+                    gradient="to top right, rgba(39,39,39,0.85), rgba(158,158,158,0.7), rgba(33,33,33,0.85)"
+                    width="100%"
+                    style="height: 100vh;"
+                >
+                    <v-row
+                        class="d-flex fill-height align-end justify-end"
+                    >
+                        <v-col>
+                            <v-container>
+                                <v-row class="d-flex justify-end align-end">
+                                    <v-col cols="10">
+                                        <h1
+                                            class="text-h5 text-lg-h4 text-white mb-6"
+                                        >
+                                           ¡Haciendo a la gente exitosa en un mundo cambiante!
+                                        </h1>
+                                        <p
+                                            class="text-body-1 text-white text-muted mb-8"
+                                        >
+                                            Mi propósito es acompañar, formar e inspirar a personas y organizaciones para
+                                            que se transformen, se adapten e innoven estratégicamente, utilizando la
+                                            tecnología, el conocimiento y la experiencia como motores del éxito
+                                            sostenible.
+                                        </p>
+
+                                        <Link
+                                            class="text-decoration-none"
+                                            href="/about"
+                                        >
+                                            <v-btn
+                                                class="mt-4 text-capitalize"
+                                                color="white"
+                                                dark
+                                                depressed
+                                                size="x-large"
+                                                variant="outlined"
+                                            >
+                                                Saber más
+                                            </v-btn>
+                                        </Link>
+                                    </v-col>
+                                </v-row>
+                            </v-container>
+                        </v-col>
+                    </v-row>
+                </v-img>
+            </v-col>
+            <v-col
+                :order="smAndDown ? '1' : '2'"
+                class="d-flex align-center bg-accent"
+                cols="12"
+                lg="5"
+                md="6"
+                style="overflow-y: auto; max-height: 100vh;"
+            >
+                <v-container class="py-0 my-0">
+                    <slot></slot>
+                </v-container>
+            </v-col>
+        </v-row>
+    </v-app>
+</template>
+
+<style>
+.v-field--variant-outlined .v-field__outline__notch {
+    flex: none;
+    position: relative;
+    border-width: 0px;
+}
+</style>
--- resources/js/Pages/Auth/ResetPassword.vue
+++ resources/js/Pages/Auth/ResetPassword.vue
@@ -1,83 +1,108 @@
 <script setup>
-import { Head, useForm } from '@inertiajs/vue3';
-import AuthenticationCard from '@/Components/AuthenticationCard.vue';
-import AuthenticationCardLogo from '@/Components/AuthenticationCardLogo.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
-import TextInput from '@/Components/TextInput.vue';
+import { nextTick, ref } from "vue";
+import { Head, useForm } from "@inertiajs/vue3";
+import AuthenticationCard from "@/Components/AuthenticationCard.vue";
+import AuthenticationCardLogo from "@/Components/AuthenticationCardLogo.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import TextInput from "@/Components/TextInput.vue";
 
-const props = defineProps({
-    email: String,
-    token: String,
-});
+const recovery = ref(false);
 
 const form = useForm({
-    token: props.token,
-    email: props.email,
-    password: '',
-    password_confirmation: '',
+    code: "",
+    recovery_code: "",
 });
 
+const recoveryCodeInput = ref(null);
+const codeInput = ref(null);
+
+const toggleRecovery = async () => {
+    recovery.value ^= true;
+
+    await nextTick();
+
+    if (recovery.value) {
+        recoveryCodeInput.value.focus();
+        form.code = "";
+    } else {
+        codeInput.value.focus();
+        form.recovery_code = "";
+    }
+};
+
 const submit = () => {
-    form.post(route('password.update'), {
-        onFinish: () => form.reset('password', 'password_confirmation'),
-    });
+    form.post(route("two-factor.login"));
 };
 </script>
 
 <template>
-    <Head title="Reset Password" />
+    <Head title="Two-factor Confirmation" />
 
     <AuthenticationCard>
         <template #logo>
             <AuthenticationCardLogo />
         </template>
 
+        <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
+            <template v-if="!recovery">
+                Please confirm access to your account by entering the
+                authentication code provided by your authenticator application.
+            </template>
+
+            <template v-else>
+                Please confirm access to your account by entering one of your
+                emergency recovery codes.
+            </template>
+        </div>
+
         <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+            <div v-if="!recovery">
+                <InputLabel for="code" value="Code" />
                 <TextInput
-                    id="email"
-                    v-model="form.email"
-                    type="email"
+                    id="code"
+                    ref="codeInput"
+                    v-model="form.code"
+                    type="text"
+                    inputmode="numeric"
                     class="mt-1 block w-full"
-                    required
                     autofocus
-                    autocomplete="username"
+                    autocomplete="one-time-code"
                 />
-                <InputError class="mt-2" :message="form.errors.email" />
+                <InputError class="mt-2" :message="form.errors.code" />
             </div>
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+            <div v-else>
+                <InputLabel for="recovery_code" value="Recovery Code" />
                 <TextInput
-                    id="password"
-                    v-model="form.password"
-                    type="password"
+                    id="recovery_code"
+                    ref="recoveryCodeInput"
+                    v-model="form.recovery_code"
+                    type="text"
                     class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
+                    autocomplete="one-time-code"
                 />
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password_confirmation" value="Confirm Password" />
-                <TextInput
-                    id="password_confirmation"
-                    v-model="form.password_confirmation"
-                    type="password"
-                    class="mt-1 block w-full"
-                    required
-                    autocomplete="new-password"
-                />
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
+                <InputError class="mt-2" :message="form.errors.recovery_code" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
-                <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Reset Password
+                <button
+                    type="button"
+                    class="text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 underline cursor-pointer"
+                    @click.prevent="toggleRecovery"
+                >
+                    <template v-if="!recovery"> Use a recovery code </template>
+
+                    <template v-else> Use an authentication code </template>
+                </button>
+
+                <PrimaryButton
+                    class="ms-4"
+                    :class="{ 'opacity-25': form.processing }"
+                    :disabled="form.processing"
+                >
+                    Log in
                 </PrimaryButton>
             </div>
         </form>
